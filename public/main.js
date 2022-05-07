30b8a2c13e51b5fa50a0343e0061c48d4596f4a6
dosya <vicdosya@gmail.com>
2022-05-07 18:22:07 +0300

    Add input validation system with CSS styling

--- public/main.js
+++ public/main.js
@@ -3,6 +3,7 @@ const titleInput = document.getElementById('titleInput');
 const authorInput = document.getElementById('authorInput');
 const contentInput = document.getElementById('contentInput');
 const blogContainer = document.getElementById('blogsContainer');
+const errorMessage = document.getElementById('errorMessage');
 
 const fetchAllPosts = async () => {
     const response = await fetch('/api/posts');
@@ -17,7 +18,7 @@ const fetchAllPosts = async () => {
 
         const blogPostAuthor = document.createElement('p');
         blogPostAuthor.setAttribute("id", "blogPostAuthor");
-        
+
         const blogPostContent = document.createElement('p');
         blogPostContent.setAttribute("id", "blogPostContent");
 
@@ -36,9 +37,10 @@ const fetchAllPosts = async () => {
 fetchAllPosts();
 
 submitButton.addEventListener('click', async () => {
+
     submitButton.disabled = true;
     submitButton.style.backgroundColor = "gray";
-    
+
     const response = await fetch('/api/posts', {
         method: "post",
         headers: {
@@ -52,13 +54,73 @@ submitButton.addEventListener('click', async () => {
         }),
 
     });
-    titleInput.value = '';
-    authorInput.value = '';
-    contentInput.value = '';
     const result = await response.json();
+
+    //deal with form inputs on error or success + CSS input styling and error messages
+    if (result.message == "error_all") {
+        NoClearForm();
+        errorMessage.innerText = "❗ Error on all the inputs.";
+        titleInput.style.border = "1px solid #fc3d03";
+        authorInput.style.border = "1px solid #fc3d03";
+        contentInput.style.border = "1px solid #fc3d03";
+
+    } else if (result.message == "error_title") {
+        errorMessage.innerText = "❗ Title must have English alphabet, numbers and 3-100 characters.";
+        titleInput.style.border = "1px solid #fc3d03";
+        authorInput.style.border = "";
+        contentInput.style.border = "";
+
+    } else if (result.message == "error_author") {
+        errorMessage.innerText = "❗ Author must have English alphabet only and 3-15 characters.";
+        titleInput.style.border = "";
+        authorInput.style.border = "1px solid #fc3d03";
+        contentInput.style.border = "";
+
+    } else if (result.message == "error_content") {
+        errorMessage.innerText = "❗ Content can have all characters, symbols, numbers and 3-3,000 characters.";
+        titleInput.style.border = "";
+        authorInput.style.border = "";
+        contentInput.style.border = "1px solid #fc3d03";
+
+    } else if (result.message == "error_title_author") {
+        errorMessage.innerText = "❗ Invalid Title and Author input.";
+        titleInput.style.border = "1px solid #fc3d03";
+        authorInput.style.border = "1px solid #fc3d03";
+        contentInput.style.border = "";
+
+    } else if (result.message == "error_title_content") {
+        errorMessage.innerText = "❗ Invalid Title and Content input.";
+        titleInput.style.border = "1px solid #fc3d03";
+        authorInput.style.border = "";
+        contentInput.style.border = "1px solid #fc3d03";
+
+    } else if (result.message == "error_author_content") {
+        errorMessage.innerText = "❗ Invalid Author and Content input.";
+        titleInput.style.border = "";
+        authorInput.style.border = "1px solid #fc3d03";
+        contentInput.style.border = "1px solid #fc3d03";
+
+    } else {
+        ClearForm();
+        errorMessage.innerText = "";
+        titleInput.style.border = "";
+        authorInput.style.border = "";
+        contentInput.style.border = "";
+    }
     console.log(result);
 
     fetchAllPosts();
 });
 
+//clear form functionality//
+const NoClearForm = function () {
+    titleInput.value;
+    authorInput.value;
+    contentInput.value;
+};
+const ClearForm = function () {
+    titleInput.value = '';
+    authorInput.value = '';
+    contentInput.value = '';
+};
 
--- server.js
+++ server.js
@@ -16,12 +16,55 @@ app.get("/", (request, response) => {
 //client's data sent to the server//
 const posts = [];
 
+//validation patterns
+const validPatterns = {
+    title: /^[a-z\d]{3,100}$/i,
+    author: /^[a-z ]{3,15}$/i,
+    content: /^[a-z\d \W]{3,3000}$/i
+};
+
 app.post('/api/posts', (req, res) => {
-    console.log(req.body);
-    posts.push(req.body);
-    res.send({
-        message: "success",
-    });
+
+    const validTitle = validPatterns.title.test(req.body.title);
+    const validAuthor = validPatterns.author.test(req.body.author);
+    const validContent = validPatterns.content.test(req.body.content);
+
+    //validation system + for CSS styling//
+    if (validTitle && validAuthor && validContent) {
+        console.log(req.body);
+        posts.push(req.body);
+        res.send({
+            message: "success",
+        });
+    } else if (!validTitle && !validAuthor && !validContent) {
+        res.send({
+            message: "error_all",
+        });
+    } else if (!validTitle && validAuthor && validContent) {
+        res.send({
+            message: "error_title",
+        });
+    } else if (validTitle && !validAuthor && validContent) {
+        res.send({
+            message: "error_author",
+        });
+    } else if (validTitle && validAuthor && !validContent) {
+        res.send({
+            message: "error_content",
+        });
+    } else if (!validTitle && !validAuthor && validContent){
+        res.send({
+            message: "error_title_author",
+        });
+    } else if(!validTitle && validAuthor && !validContent){
+        res.send({
+            message: "error_title_content",
+        });
+    } else if(validTitle && !validAuthor && !validContent){
+        res.send({
+            message: "error_author_content",
+        });
+    }
 });
 
 app.get('/api/posts', (req, res) => {
